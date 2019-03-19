5169eccde4719bb8f567a3b4d1a5bbc3ead575bf
Gunay <lambertalv@gmail.com>
2019-03-19 16:24:55 +0700

    Проблемы с модальным окном

--- js/script.js
+++ js/script.js
@@ -52,5 +52,63 @@ sliderInputNext.addEventListener('click', function() {
   next();
 })
 
+var servInputs = document.querySelectorAll('.services__input');
+var servSlideFirst = document.querySelector('.services__slide--first');
+var servSlideSecond = document.querySelector('.services__slide--second');
+var servSlideThird = document.querySelector('.services__slide--third');
+
+servInputs[0].addEventListener('click', function() {
+  slider(servSlideFirst, servSlideSecond, servSlideThird);
+});
+
+servInputs[1].addEventListener('click', function() {
+  slider(servSlideSecond, servSlideFirst, servSlideThird);
+});
+
+servInputs[2].addEventListener('click', function() {
+  slider(servSlideThird, servSlideFirst, servSlideSecond);
+});
+
+function slider(sliderShown, sliderRemoved1, sliderRemoved2) {
+  sliderRemoved1.classList.remove('services__slide--shown');
+  sliderRemoved2.classList.remove('services__slide--shown');
+  sliderShown.classList.add('services__slide--shown');
+};
 
 
+var mapImg = document.querySelector('.about-us__map-img');
+var mapIframe = document.querySelector('.about-us__contacts-popup');
+var mapClose = document.querySelector('.about-us__popup-close');
+
+mapImg.addEventListener('click', function() {
+  mapIframe.style.display = 'block';
+});
+
+mapClose.addEventListener('click', function() {
+  mapIframe.style.display = 'none';
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    mapIframe.style.display = 'none';
+    overlay.style.display = 'none';
+    contactsForm.classList.remove('form--showed');
+  }
+});
+
+
+var contactsBtn = document.querySelector('.about-us__button--contacts');
+var contactsForm = document.querySelector('.form');
+var contactsClose = document.querySelector('.form__buttom');
+var overlay = document.querySelector('.overlay');
+
+contactsBtn.addEventListener('click', function() {
+  overlay.style.display = 'block';
+  contactsForm.classList.add('form--showed');
+})
+
+overlay.addEventListener('click', function() {
+  this.style.display = 'none';
+  contactsForm.classList.remove('form--showed');
+})
